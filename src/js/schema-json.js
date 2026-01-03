d1ac18b7647f0f43d8a69dadfe9d90714f96ee9a
idlehands1969 <109295206+idlehands1969@users.noreply.github.com>
2026-01-03 12:55:50 -0600

    Update schema-json and main script

--- src/js/schema-json.js
+++ src/js/schema-json.js
@@ -1,6 +1,6 @@
 // src/js/schema-json.js
 // This file is generated at build time
-const schemaJson = "";
+const schemaJson = '';
 if (schemaJson) {
   const script = document.createElement('script');
   script.type = 'application/ld+json';
